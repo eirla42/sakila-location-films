af3a48f31b5a342a545a46908c9645293c69c780
eirla42 <math.poke@gmail.com>
2022-02-04 09:18:56 +0100

    MVVM Architecture

---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Database access
+const BD_HOST = '';
+const BD_DBNAME = '';
+const BD_USER = '';
+const BD_PWD = '';
+
+// Language of website
+const LANG ='FR-fr';
+
+// Other parameters
+const AUTHORS = 'Claude & Mathieu & Maxime';
+const TITLE = 'Sakila - Location de films';
