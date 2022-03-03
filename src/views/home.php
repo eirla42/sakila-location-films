3c85d53f228fd9d9678b5bb8dc8b5cb6e48a5830
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2022-03-03 10:16:36 +0100

    Add Actor List to home page

--- src/views/home.php
+++ src/views/home.php
@@ -18,4 +18,19 @@ require __DIR__ . '/../views-models/home.php';
         </tbody>
     </table>
 
+    <h1>Liste des acteurs</h1>
+
+    <table class="table table-bordered table-condensed">
+        <thead>
+        <tr>
+            <th>Titre</th>
+            <th>Description</th>
+            <th>Année</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php renderActors(); ?>
+        </tbody>
+    </table>
+
 <?php require_once 'footer.php'; ?>
