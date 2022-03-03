970b4acfb16b4749bcf53e08f3d204b8f9d1c851
eirla42 <math.poke@gmail.com>
2022-03-03 10:49:30 +0100

    Form add film - basic

--- src/views/admin/film/add-film.php
+++ src/views/admin/film/add-film.php
@@ -1,1 +1,51 @@
 <?php
+require_once '../../header.php';
+require __DIR__ .'/../../../db/db-connect.php';
+require __DIR__ . '/../../../views-models/home.php';
+
+?>
+    <h1>Ajouter un film</h1>
+
+    <form>
+        <div class="mb-3 row">
+            <div class="col-md-8">
+                <label for="title" class="form-label">Titre</label>
+                <input type="text" class="form-control" id="title">
+            </div>
+
+            <div class="col-md-4">
+                <label for="release_year" class="form-label">Année de sortie</label>
+                <input type="number" class="form-control" id="release_year">
+            </div>
+        </div>
+
+        <div class="mb-3 row">
+            <div class="col-md-12">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description"></textarea>
+            </div>
+        </div>
+
+        <div class="mb-3 row">
+            <div class="col-md-6">
+                <select class="form-select" aria-label="Langue">
+                    <option selected>Selectionner une langue</option>
+                    <option value="1">Anglais</option>
+                    <option value="2">Italien</option>
+                    <option value="3">Japonnais</option>
+                </select>
+            </div>
+
+            <div class="col-md-6">
+                <select class="form-select" aria-label="Langue">
+                    <option selected>Selectionner une langue d'origine</option>
+                    <option value="1">Anglais</option>
+                    <option value="2">Italien</option>
+                    <option value="3">Japonnais</option>
+                </select>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+<?php require_once '../../footer.php'; ?>
--- src/views/header.php
+++ src/views/header.php
@@ -1,13 +1,15 @@
 <?php
-    require __DIR__."/../config/config.php";
+    require_once __DIR__."/../config/config.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
-          integrity='sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z' crossorigin='anonymous'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/style.css">
     <title> <?= TITLE; ?> </title>
 </head>
@@ -16,7 +18,7 @@
     <nav>
         <ul>
             <li><a href="<?= URL_ROOT; ?>"> Accueil </a></li>
-            <li><a href="<?= URL_ROOT; ?>views/admin/film/list-film.php"> Liste des films </a></li>
+            <li><a href="<?= URL_ROOT; ?>views/admin/film/list-film.php"> Admin - Liste des films </a></li>
         </ul>
     </nav>
 
