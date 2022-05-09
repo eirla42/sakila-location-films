a30ccd9ffe6a86f03a14c87c14590204e2516e59
eirla42 <math.poke@gmail.com>
2022-05-09 14:41:54 +0200

    Add film

---
+++ src/views-models/admin/film/add-film.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/../../../models/Film.php';
+
+
+// INSERT film
+function insertFilm() {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $special_features = '';
+        for ($i=1; $i<=4; $i++) {
+            if(isset($_POST['check'.$i])) {
+                if ($special_features === '') {
+                    $special_features = $special_features . $_POST['check'.$i];
+                } else {
+                    $special_features =  $special_features . ',' .$_POST['check'.$i];
+                }
+            }
+        }
+        $film = new Film(0, $_POST['title'], $_POST['description'], $_POST['release_year'], $_POST['language_id'], $_POST['original_language_id'], $_POST['rental_duration'],
+            $_POST['rental_rate'], $_POST['length'], $_POST['replacement_cost'], $_POST['rating'], $special_features, 0);
+        $film->add();
+
+        // Go to page index.php
+        header('Location: __DIR__ ./../../../admin/film/list-film.php');
+    }
+}
