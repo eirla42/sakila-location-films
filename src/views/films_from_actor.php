0a693870b3a04b2c75001604738260c97c968e68
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2022-05-09 22:48:17 +0200

    Add view and view model for request films from actor name

---
+++ src/views/films_from_actor.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'header.php';
+require __DIR__ . '/../views-models/films_from_actor.php';
+require_once '../db/db-connect.php';
+
+?>
+<h1>Liste des films par Acteur</h1>
+
+<table class="table table-bordered table-condensed">
+    <thead>
+    <tr>
+        <th>Titre</th>
+        <th>Description</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php renderFilmsFromActor('PALTROW', 'KIRSTEN'); ?>
+    </tbody>
+</table>
+
+<?php require_once 'footer.php'; ?>
