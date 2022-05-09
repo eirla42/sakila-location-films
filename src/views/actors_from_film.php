2eccfae1b43996576c4149873f348b58fd9a0392
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2022-05-09 21:59:52 +0200

    add page to display all actors from film name

---
+++ src/views/actors_from_film.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'header.php';
+require __DIR__ . '/../views-models/home.php';
+require_once '../db/db-connect.php';
+
+?>
+<h1>Liste des acteurs</h1>
+
+<table class="table table-bordered table-condensed">
+    <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Prénom</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php renderActorsFromFilm('AGENT TRUMAN'); ?>
+    </tbody>
+</table>
+
+<?php require_once 'footer.php'; ?>
